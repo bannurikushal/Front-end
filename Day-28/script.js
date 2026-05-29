fd440d85cd459bc1d9ad8f34103f8254c5225d47
bannurikushal <bannurikushal59@gmail.com>
2026-05-29 22:14:40 +0530

    Day-28 files uploaded

---
+++ Day-28/script.js
@@ -0,0 +1,17 @@
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(10, 20);
+// function even(num) {
+//   console.log(num % 2 === 0 ? "Even" : "Odd");
+// }
+// even(10);
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    console.log("number is factorial n");
+  }
+  factorial(5);
+}
